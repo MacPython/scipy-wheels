f0809f5574b24fed502c438b34107f92fc2906c7
Matthew Brett <matthew.brett@gmail.com>
2016-06-19 14:20:02 -0700

    Fix up tests and wheel dependencies

--- config.sh
+++ config.sh
@@ -8,16 +8,23 @@ function pre_build {
         sudo installer -pkg archives/gfortran-4.2.3.pkg -target /
     else
         build_openblas
+        # Force scipy to use OpenBLAS regardless of what numpy uses
+        cat << EOF > $HOME/site.cfg
+[openblas]
+library_dirs = /usr/local/lib
+include_dirs = /usr/local/include
+EOF
     fi
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
+    test_cmd="import sys; import scipy; sys.exit(not scipy.test('full').wasSuccessful())"
     if [ -n "$IS_OSX" ]; then  # Test both architectures on OSX
-        arch -i386 python -c 'import scipy; scipy.test("full")'
-        arch -x86_64 python -c 'import scipy; scipy.test("full")'
-    else  # Test both architectures on OSX
-        python -c 'import scipy; scipy.test("full")'
+        arch -i386 python -c "$test_cmd"
+        arch -x86_64 python -c "$test_cmd"
+    else  # Not OSX
+        python -c "$test_cmd"
     fi
     # Show BLAS / LAPACK used
     python -c 'import scipy; scipy.show_config()'
