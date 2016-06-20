e6c40025b047ed7af7bc76d9d10e2ae337a42268
Matthew Brett <matthew.brett@gmail.com>
2016-06-20 12:10:13 -0700

    Reduce verbosity of openblas, no full tests on OSX

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ function pre_build {
     if [ -n "$IS_OSX" ]; then
         sudo installer -pkg archives/gfortran-4.2.3.pkg -target /
     else
-        build_openblas
+        build_openblas >& /dev/null
         # Force scipy to use OpenBLAS regardless of what numpy uses
         cat << EOF > $HOME/site.cfg
 [openblas]
@@ -19,11 +19,13 @@ EOF
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
-    test_cmd="import sys; import scipy; sys.exit(not scipy.test('full').wasSuccessful())"
     if [ -n "$IS_OSX" ]; then  # Test both architectures on OSX
+        # Can't afford full tests when running both test rigs in same job
+        test_cmd="import sys; import scipy; sys.exit(not scipy.test().wasSuccessful())"
         arch -i386 python -c "$test_cmd"
         arch -x86_64 python -c "$test_cmd"
     else  # Not OSX
+        test_cmd="import sys; import scipy; sys.exit(not scipy.test('full').wasSuccessful())"
         python -c "$test_cmd"
     fi
     # Show BLAS / LAPACK used
