90ef810d50414988cec51f435bac230cce4a5662
Matthew Brett <matthew.brett@gmail.com>
2016-06-20 18:35:40 -0700

    Download openblas instead of building it each time

--- config.sh
+++ config.sh
@@ -6,15 +6,18 @@ function pre_build {
     # Runs in the root directory of this repository.
     if [ -n "$IS_OSX" ]; then
         sudo installer -pkg archives/gfortran-4.2.3.pkg -target /
-    else
-        build_openblas > /dev/null
-        # Force scipy to use OpenBLAS regardless of what numpy uses
-        cat << EOF > $HOME/site.cfg
+        return
+    fi
+    # Rackspace builds of openblas
+    local lib_url=https://d9a97980b71d47cde94d-aae005c4999d7244ac63632f8b80e089.ssl.cf2.rackcdn.com
+    local lib_tgz="openblas_0.2.18-$(get_platform).tgz"
+    (cd / && curl -LO $lib_url/${lib_tgz} && tar zxf $lib_tgz)
+    # Force scipy to use OpenBLAS regardless of what numpy uses
+    cat << EOF > $HOME/site.cfg
 [openblas]
 library_dirs = /usr/local/lib
 include_dirs = /usr/local/include
 EOF
-    fi
 }
 
 function run_tests {
