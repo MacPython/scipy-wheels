02d41642441851d65a0b60f3bd83f1243c709e47
Pauli Virtanen <pav@iki.fi>
2017-09-16 15:56:17 +0200

    MAINT: check license file in installed distribution

--- patch_code.sh
+++ patch_code.sh
@@ -4,6 +4,11 @@ repo_dir=${1:-$REPO_DIR}
 
 source multibuild/common_utils.sh
 
+if [ ! -f $repo_dir/LICENSE.txt ]; then
+    echo "ERROR: License file in $repo_dir/ missing"
+    exit 1
+fi
+
 if [ -z "$IS_OSX" ]; then
     cat LICENSE_linux.txt >> $repo_dir/LICENSE.txt
 else
