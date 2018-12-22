a39a0f7f95fdadee25d028fcb6ca527e120a37ea
Charles Harris <charlesr.harris@gmail.com>
2018-12-22 12:03:22 -0700

    Update the OpenBLAS library.
    Update to the fixed up v0.3.4 version, which is sadly named as being
    v0.3.3. The newer library fixes many threading problems found in the
    earlier version as well as other bugs.

--- env_vars.sh
+++ env_vars.sh
@@ -1,5 +1,5 @@
 # Environment variables for build
-OPENBLAS_VERSION="v0.3.0"
+OPENBLAS_VERSION="v0.3.3-186-g701ea883"
 MACOSX_DEPLOYMENT_TARGET=10.6
 
 # Enable Python fault handler on Pythons >= 3.3.
