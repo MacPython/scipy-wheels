fb417fec1c30804925cc4a1d166deacc5530fa35
Tyler Reddy <tyler.je.reddy@gmail.com>
2020-04-26 18:04:01 -0600

    DEBUG: try moving into DLL dir
    * try to solve DLL resolution issues for older
    (32-bit) Python versions by temporarily moving into the
    path where they are stored prior to load attempts
    in `_distributor_init.py`

--- _distributor_init.py
+++ _distributor_init.py
@@ -24,6 +24,13 @@ if os.name == 'nt':
     print("**libs_path:", libs_path)
     if os.path.isdir(libs_path):
         print("files in libs_path:", os.listdir(libs_path))
+        # for Python < 3.8, try to achieve brute force resolution
+        # of DLLs by moving into their storage dir; should have no
+        # effect for Python >= 3.8
+        owd = os.getcwd()
+        os.chdir(libs_path)
         for filename in glob.glob(os.path.join(libs_path, '*dll')):
             print("**filename:", filename)
             WinDLL(os.path.abspath(filename))
+
+        os.chdir(owd)
