7470cdbfa20ad371789d5c1bb22a78f9808792f1
Ralf Gommers <ralf.gommers@gmail.com>
2021-11-28 12:51:45 +0100

    BUG: fix inclusion of `_distributor_init.py` on macOS.
    Follow-up to gh-145

--- patch_code.sh
+++ patch_code.sh
@@ -13,5 +13,5 @@ if [ -z "$IS_OSX" ]; then
     cat LICENSE_linux.txt >> $repo_dir/LICENSE.txt
 else
     cat LICENSE_osx.txt >> $repo_dir/LICENSE.txt
-    cp _distributor_init.py scipy/_distributor_init.py
+    cp _distributor_init.py scipy/scipy/_distributor_init.py
 fi
